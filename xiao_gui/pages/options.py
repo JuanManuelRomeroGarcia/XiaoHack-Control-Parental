2e0d7f6c21478723c7efbf2aebd3cf2aa1e06213
Juan Manuel Romero García <xiaohack@xiaohack.es>
2025-11-07 19:58:13 +0100

    update py portable

--- xiao_gui/pages/options.py
+++ xiao_gui/pages/options.py
@@ -116,7 +116,7 @@ class OptionsPage(ttk.Frame):
     # ---------------- Hooks de navegación ----------------
     def on_show_async(self, rev=None):
         rev = self._gate.next_rev() if rev is None else rev
-        submit_limited(self._task_refresh_service, rev)
+        submit_limited(self.refresh_service, rev)
         self.after(450, self.refresh_notifier_task_info)
         self.after(1200, self._auto_check_updates)
 
